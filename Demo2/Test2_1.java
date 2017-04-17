b97fd2a9e94954eb897082fd20cb1ddf481d5646
LXTaven <LXTaven@users.noreply.github.com>
2017-04-17 13:00:43 +0800

    Create Test2_1.java

---
+++ Demo2/Test2_1.java
@@ -0,0 +1,19 @@
+package Demo2;
+//字符常量
+//字符串常量
+
+public class Test2_1 
+{
+
+	public static void main(String [] args)
+	{
+		System.out.println(5);
+		System.out.println(0x3c);  //十六进制
+		System.out.println(Integer.toBinaryString(-100997866)); //在屏幕输出二进制数据
+		
+		
+		
+		
+	}
+
+}
