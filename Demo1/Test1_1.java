2db3c43019f62e790eab7616c6c4f5b82a207f4a
LXTaven <LXTaven@users.noreply.github.com>
2017-04-17 12:59:57 +0800

    Create Test1_1.java

---
+++ Demo1/Test1_1.java
@@ -0,0 +1,40 @@
+package Demo1;
+//类里面的主函数main不是必须的
+//在一个文件中只能有一个public的类
+
+//main函数可以保证该类独立运行
+//是程序的入口
+//被jvm调用
+
+
+public class Test1_1 
+{
+	public static void main(String [] args)
+	{
+		System.out.println("hello java");
+	
+	}
+
+}
+//这是单行注释
+/*多行注释*/
+
+
+
+
+ class Test1_2   //class——java中的关键字  用来定义类   大小写
+{
+	public static void main(String [] args)
+	{
+		System.out.println("hello java");
+		
+	}
+
+}
+
+ 
+ 
+ 
+ 
+ 
+ 
