5d80105d08c014973f41bd646231612f5413e3e9
LXTaven <LXTaven@users.noreply.github.com>
2017-04-18 15:32:00 +0800

    Create Test2_2.java

---
+++ Demo2/Test2_2.java
@@ -0,0 +1,58 @@
+package Demo2;
+//变量：就是将不确定的数字进行存储
+//需要在内存中开辟一个空间
+//需要明确数据类型，变量名称  数据来完成
+//定义变量的格式
+//数据类型 变量名 = 初始化值
+public class Test2_2 
+{
+	//static int x = 5;
+	public static void main (String [] args)
+	{
+		int x = 5;
+		
+		
+		System.out.println(x);
+		x=x+5;
+		System.out.println(x);
+		
+		
+		
+		byte b = 2;
+		
+		short s = 30000;
+		
+		long l = 100l; //需要用”l“来进行标识
+		
+		float f = 2.3f; //小数默认是double
+		
+		String ch = "5454";
+		char ch1 = '4';
+		
+		
+		boolean bo1 = false;
+		boolean bo2 = true;
+		
+		System.out.print(ch);
+		
+		
+		/*
+		 * 
+		 * 什么时候定义变量：当数据不确定时   需要对数据进行存储时
+		 * 就需要定义一个变量来完成存储动作
+		 * 
+		 * */
+		
+				
+		
+		
+		
+		
+		
+		 
+		
+		
+		
+	}
+
+}
