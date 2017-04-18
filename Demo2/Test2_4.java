b6ac671264136fdf329216633e2cb30abb803b28
LXTaven <LXTaven@users.noreply.github.com>
2017-04-18 15:33:13 +0800

    Create Test2_4.java

---
+++ Demo2/Test2_4.java
@@ -0,0 +1,17 @@
+package Demo2;
+//运算符
+//字符串数据和任何数据使用+都是相连接 ，最终都会变成字符串
+public class Test2_4 
+{
+	public static void main(String [] args)
+	{
+		int x=4242;
+		x=x/1000*1000;
+		System.out.println(x);
+		System.out.println(10%3);
+		
+		System.out.println("dsdsds"+"ffdfd");
+		
+		
+	}
+}
