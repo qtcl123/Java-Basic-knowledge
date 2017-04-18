a6157aebfcfefe63a6fe6a46c0b21fa8e2d3590f
LXTaven <LXTaven@users.noreply.github.com>
2017-04-18 15:34:35 +0800

    Create Test2_7.java

---
+++ Demo2/Test2_7.java
@@ -0,0 +1,31 @@
+package Demo2;
+//只要有一个满足直接跳出
+public class Test2_7 
+{
+	public static void main (String[] args)
+	{
+		int n = -1;
+		if (n>1)
+		{
+			System.out.println("a");
+		}
+		else if (n>2)
+		{
+			System.out.println("b");
+		}
+		else if (n>3)
+		{
+			System.out.println("c");
+		}
+		else 
+		{
+			System.out.println("不满足");
+			
+		}
+		
+	
+	}
+	
+	
+
+}
