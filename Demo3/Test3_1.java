5b6f13a73ac1c3567b8f82978551c0358a164943
LXTaven <LXTaven@users.noreply.github.com>
2017-04-18 15:36:25 +0800

    Create Test3_1.java

---
+++ Demo3/Test3_1.java
@@ -0,0 +1,31 @@
+package Demo3;
+//while 讲解
+
+//循环语句是满足条件一直循环
+//控制语句是有一次满足即可
+public class Test3_1 {
+	public static void main(String [] args)
+	{
+		
+		/*
+		 *定义初	始化的表达式；
+		 *while(条件表达式)
+		 *{
+		 *循环体（执行语句）
+		 *} 
+		 * */
+		
+		int x = 1;
+		while (x<10)
+		{
+			System.out.println("x="+x); 
+			x++;
+		
+		}
+		
+		
+	
+	}
+
+}
+
