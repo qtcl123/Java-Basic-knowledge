857eb3cff85822d07bb86657296896b14cd5e201
LXTaven <LXTaven@users.noreply.github.com>
2017-04-18 15:38:20 +0800

    Create Test3_5.java

---
+++ Demo3/Test3_5.java
@@ -0,0 +1,44 @@
+package Demo3;
+//循环语句的其他特点
+public class Test3_5 
+{
+	public static void main(String [] args)
+	{
+		int x = 1;
+		for(System.out.println("a");x<3;System.out.println("c"))
+		{
+			System.out.println("d");
+			x++;
+			
+		}
+		
+	
+		//注：在for中初始化表达式可以不写   
+		//第四步也可以不写
+		
+		
+		/**
+		 * 无限循环的最简单的表现形式：
+		 * for(;;) ()
+		 * 
+		 *while (True) ()
+		 *
+		 *
+		 * 
+		 * 
+		 */
+		
+		
+			
+		
+				
+		
+		
+	}
+	
+
+}
+
+
+
+
