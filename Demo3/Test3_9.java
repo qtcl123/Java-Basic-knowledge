0c3cf34b190bce79bade4a6ebe698cde008d41f4
LXTaven <LXTaven@users.noreply.github.com>
2017-04-18 15:40:22 +0800

    Create Test3_9.java

---
+++ Demo3/Test3_9.java
@@ -0,0 +1,36 @@
+package Demo3;
+//九九乘法表
+public class Test3_9 
+{
+	public static void main(String[] args)
+	{
+		for(int x=1;x<=9;x++)
+		{
+			for (int y=1;y<=x;y++)
+			{
+				System.out.print(y+"*"+x+"="+y*x+"\t");
+				
+			}
+			
+			System.out.println();
+		}
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
