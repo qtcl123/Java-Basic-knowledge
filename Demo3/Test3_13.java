ec93a32b6076bedf30a672218bad1b86320f13dd
LXTaven <LXTaven@users.noreply.github.com>
2017-04-18 15:42:24 +0800

    Create Test3_13.java

---
+++ Demo3/Test3_13.java
@@ -0,0 +1,29 @@
+package Demo3;
+//语句练习
+public class Test3_13 
+{
+	public static void main(String[] args)
+	{
+		for(int x=0;x<5;x++)
+		{
+			for(int y=x;y<5;y++)
+			{
+				System.out.print("-");
+				
+			}
+			
+			for (int z=0;z<=x;z++)
+			{
+				System.out.print("* ");
+				
+			}
+			
+			System.out.println();
+			
+			
+			
+		}
+	}
+
+}
+	
