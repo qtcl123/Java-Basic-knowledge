51e7bc38fabf047a407498f1454959f0b6344d98
LXTaven <LXTaven@users.noreply.github.com>
2017-04-18 15:34:09 +0800

    Create Test2_6.java

---
+++ Demo2/Test2_6.java
@@ -0,0 +1,28 @@
+package Demo2;
+//流程控制
+//三元运算符：
+//好处：可以简化if else 代码
+//弊端：因为是一个运算符   所以必须要有结果
+
+public class Test2_6 
+{
+	public static void main(String [] args)
+	{
+		int x = 3;
+		if (x>3)
+		{
+			System.out.println(1);
+			
+		}
+		else 
+		{
+			System.out.println(2);
+		}
+		
+		System.out.println("game over");  //该语句和if没关
+		
+		
+		
+		
+	}
+}
