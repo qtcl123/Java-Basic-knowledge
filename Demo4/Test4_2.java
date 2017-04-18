f0b809635dfc47c134a1eb437c9e337e8943977c
LXTaven <LXTaven@users.noreply.github.com>
2017-04-18 15:48:08 +0800

    Create Test4_2.java

---
+++ Demo4/Test4_2.java
@@ -0,0 +1,38 @@
+package Demo4;
+//数组
+public class Test4_2 
+{
+	public static void main(String[] args)
+	{
+		//数组的几种定义方式：
+		int[] x = new int [2];
+		//数组的另外一种定义方式：int x[] = new int [2];
+		x[0] = 1;
+		
+		
+		
+		
+		int [] arr = new int[] {3,4,4,5,5,56,7,7}; //数组静态初始化方式
+		//数组的另外一种初始化方式：int [] arr = {3,4,4,5,5,56,7,7};
+		System.out.println(arr[1]);
+		//注意里面存在角标越界的情况：访问数组时   访问到数组中不存在的角标——空指针异常
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
