d41b3969d85a0553c32bcc518de0fbf846da4d2c
LXTaven <LXTaven@users.noreply.github.com>
2017-04-18 15:45:37 +0800

    Create Test3_17.java

---
+++ Demo3/Test3_17.java
@@ -0,0 +1,31 @@
+package Demo3;
+//比较两个数   返回较大的值
+public class Test3_17 
+{
+	public static void main(String [] args)
+	{
+		
+	}
+	
+	public static int bigNum(int x,int y)
+	{
+		if (x>y)
+		{
+			return x;
+		}
+		else 
+		{
+			return y;
+		}
+		
+		//这样的写法也没没有问题
+		//return (x>y)?x:y;
+		
+		
+		
+		
+		
+		
+	}
+
+}
