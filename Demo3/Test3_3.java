a7df10ef417d53aafc1823083beb145bd0ddb2f2
LXTaven <LXTaven@users.noreply.github.com>
2017-04-18 15:37:28 +0800

    Create Test3_3.java

---
+++ Demo3/Test3_3.java
@@ -0,0 +1,26 @@
+package Demo3;
+//for的讲解
+public class Test3_3 {
+	public static void main(String [] args)
+	{
+		/*
+		 * for(初始化表达式;循环条件表达式;循环后的操作表达式;)
+		 * { 
+		 * 执行语句
+		 * }
+		 * 
+		 * x = 0 该语句在空间中开辟一个空间  值为0 命名为x
+		 * 
+		 * 
+		 * 
+		 * */
+		for (int x = 0;x<3;x++)//1 2 4 步
+		{
+			System.out.println("x:"+x); //第3步
+			
+		}
+		
+		
+	}
+
+}
