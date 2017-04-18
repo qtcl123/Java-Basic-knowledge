02a7db70929e3d9b15020461852c62ddfb278724
LXTaven <LXTaven@users.noreply.github.com>
2017-04-18 15:33:36 +0800

    Create Test2_5.java

---
+++ Demo2/Test2_5.java
@@ -0,0 +1,26 @@
+package Demo2;
+//运算符
+//三元运算符
+public class Test2_5 
+{
+	public static void main(String [] args)
+	{
+	System.out.println(3==4);
+	
+	
+	int x =1200,y;
+	y = (x>100)?x = x + x:x++;
+	System.out.println(x);
+	System.out.print(y);
+	
+	
+	
+	
+			
+	
+	
+	}
+	
+}
+
+
